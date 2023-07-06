82b398790b41c359f188e6b2266596a5fac78a9d
Nik <61439310+NikV2@users.noreply.github.com>
2023-07-06 23:39:37 +0300

    Now cleans discord cache

--- src/main/java/me/nik/bootcleaner/enums/Directories.java
+++ src/main/java/me/nik/bootcleaner/enums/Directories.java
@@ -4,16 +4,28 @@ import me.nik.bootcleaner.utils.MiscUtils;
 
 public enum Directories {
     PREFETCH(MiscUtils.ROOT + "Windows\\Prefetch"),
+
     THUMBNAIL_CACHE(MiscUtils.USER + "AppData\\Local\\Microsoft\\Windows\\Explorer"),
+
     CBS_TEMP(MiscUtils.ROOT + "Windows\\CbsTemp"),
+
     WINDOWS_LOGS(MiscUtils.ROOT + "Windows\\Logs"),
+
     WINDOWS_TEMP(MiscUtils.ROOT + "Windows\\temp"),
+
     FILE_EXPLORER_CACHE(MiscUtils.USER + "AppData\\Roaming\\Microsoft\\Windows\\Recent\\AutomaticDestinations"),
+
     NET_CACHE(MiscUtils.USER + "AppData\\Local\\Microsoft\\Windows\\INetCache\\IE"),
 
     DIRECTX_SHADER_CACHE(MiscUtils.USER + "AppData\\Local\\D3DSCache"),
 
     WINDOWS_UPDATE_CACHE(MiscUtils.ROOT + "Windows\\SoftwareDistribution\\Download"),
+
+    DISCORD_CACHE(MiscUtils.USER + "AppData\\Roaming\\discord\\Cache"),
+    DISCORD_CODE_CACHE(MiscUtils.USER + "AppData\\Roaming\\discord\\Code Cache"),
+    DISCORD_GPU_CACHE(MiscUtils.USER + "AppData\\Roaming\\discord\\GPUCache"),
+    DISCORD_DAWN_CACHE(MiscUtils.USER + "AppData\\Roaming\\discord\\DawnCache"),
+
     TEMP(MiscUtils.USER + "AppData\\Local\\Temp");
 
     private final String directory;
